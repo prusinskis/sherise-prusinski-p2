8df0d253236a515eab9235300b4cf8a03926ef95
Sherise Prusinski <98294721+prusinskis@users.noreply.github.com>
2023-05-29 13:11:23 -0500

    page sorting by sortkey

--- blog/gatsby-node.js
+++ blog/gatsby-node.js
@@ -13,28 +13,28 @@ exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
   return new Promise((resolve, reject) => {
     graphql(`
-    
       query MyAllContentfulPageBlogPost {
-        allContentfulPageBlogPost {
+        allContentfulPageBlogPost(sort: { sortkey: ASC }) {
           edges {
             node {
+              sortkey
               id
               slug
             }
           }
         }
       }
-    
     `).then(result => {
       if (result.errors) {
         reject(result.errors)
       }
-      result.data.allContentfulPageBlogPost.edges.forEach((edge) => {
+      result.data.allContentfulPageBlogPost.edges.forEach(edge => {
         createPage({
           path: edge.node.slug,
           component: require.resolve("./src/templates/blog-post.js"),
           context: {
             slug: edge.node.slug,
+            sortkey: edge.node.sortkey
           },
           defer: true,
         })
